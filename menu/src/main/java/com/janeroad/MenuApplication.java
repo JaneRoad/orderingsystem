f949a0e46d47a40854a2ed367b07ac826b2fbb0b
ljn <425999212@qq.com>
2020-04-12 00:49:40 +0800

    3

--- menu/src/main/java/com/janeroad/MenuApplication.java
+++ menu/src/main/java/com/janeroad/MenuApplication.java
@@ -3,8 +3,9 @@ package com.janeroad;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.janeroad.repository")
 public class MenuApplication {
     public static void main(String[] args) {
